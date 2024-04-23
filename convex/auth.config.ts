93a9de5136b956e7023ef62a13a29e9582c45fce
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-04-23 21:11:18 +0800

    feat: include clerk domain link

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://your-issuer-url.clerk.accounts.dev/",
+        domain: "https://dynamic-ray-80.clerk.accounts.dev",
         applicationID: "convex",
       },
     ]
