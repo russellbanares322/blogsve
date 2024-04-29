ba0c6526122d08f9cfa1fe2e4556d59d0e251480
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-04-29 20:56:37 +0800

    feat: create a mutation for saving blogpost

--- convex/blogs.ts
+++ convex/blogs.ts
@@ -1,8 +1,18 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
-export const get = query({
+// Queries
+export const getBlogs = query({
     args: {},
     handler: async(ctx) => {
         return await ctx.db.query("blogs").collect()
+    }
+})
+
+// Mutations
+export const createBlog = mutation({
+    args: { title: v.string(), dateCreated: v.string(), body: v.string() },
+    handler: async (ctx, args) => {
+        return await ctx.db.insert("blogs", {...args})
     }
 })
