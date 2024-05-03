6dd9884fdffee735ed1fb982c9525ca8ec40e9be
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-03 20:22:22 +0800

    feat: remove comments in blog.ts file

--- convex/blogs.ts
+++ convex/blogs.ts
@@ -1,21 +1,11 @@
 import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
-// Implement update and deletion of blogs
 // Queries
 export const getBlogs = query({
     args: {},
     handler: async(ctx) => {
          await ctx.db.query("blogs").collect()
-    }
-})
-
-export const getBlog = query({
-    args: { blogId: v.id("blogs")},
-    handler: async(ctx, args) => {
-        const blog = await ctx.db.query("blogs").filter((q) => q.eq(q.field("_id"), args.blogId));
-
-        return blog
     }
 })
 
