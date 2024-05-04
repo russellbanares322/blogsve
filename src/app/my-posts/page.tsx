4f6817e6671f4d9b32cd2d01b5873b12ba38efb7
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-04 20:49:40 +0800

    feat: install input and label component from shadcn ui

--- src/app/my-posts/page.tsx
+++ src/app/my-posts/page.tsx
@@ -5,6 +5,7 @@ import { api } from "../../../convex/_generated/api";
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/nextjs";
 import { SquarePlus } from "lucide-react";
+import CreateBlogFormModal from "@/components/create-blog-form-modal";
 
 const MyPosts = () => {
   const { user } = useUser();
@@ -19,9 +20,7 @@ const MyPosts = () => {
       {hasNoBlogCreated && (
         <div className="flex items-center justify-center flex-col mt-10 gap-4">
           <h1>You haven't posted blog yet</h1>
-          <Button>
-            <SquarePlus className="h-4 w-4 mr-2" /> Create Blog
-          </Button>
+          <CreateBlogFormModal />
         </div>
       )}
     </div>
--- src/components/create-blog-form-modal.tsx
+++ src/components/create-blog-form-modal.tsx
@@ -1,5 +1,57 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { SquarePlus } from "lucide-react";
+
 const CreateBlogFormModal = () => {
-  return <div>Blog Form Modal</div>;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <SquarePlus className="h-4 w-4 mr-2" /> Create Blog
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create Blog</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              defaultValue="Pedro Duarte"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input
+              id="username"
+              defaultValue="@peduarte"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CreateBlogFormModal;
