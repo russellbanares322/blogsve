675e8ada8eac2c9117440514b26936814b16d35b
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-10 20:28:55 +0800

    feat: include read more text in blogs data card

--- src/app/my-posts/blogs-data-display.tsx
+++ src/app/my-posts/blogs-data-display.tsx
@@ -7,6 +7,7 @@ import {
 import { GetBlogsResponseData } from "../../../convex/blogs";
 import { truncateText } from "@/lib/truncateText";
 import moment from "moment";
+import { EllipsisVertical } from "lucide-react";
 
 type BlogsDataDisplayProps = {
   blogsData: GetBlogsResponseData;
@@ -14,14 +15,23 @@ type BlogsDataDisplayProps = {
 
 const BlogsDataDisplay = ({ blogsData }: BlogsDataDisplayProps) => {
   return (
-    <Card className="duration-75 cursor-pointer hover:shadow-md">
+    <Card className="duration-75 hover:shadow-md relative">
       <CardHeader>
         <CardTitle>{blogsData.title}</CardTitle>
         <small className="font-light">
           {moment(blogsData._creationTime).format("LL")}
         </small>
-        <CardDescription>{truncateText(blogsData.description)}</CardDescription>
+        <CardDescription>
+          {truncateText(blogsData.description)}{" "}
+          <span className="italic text-xs cursor-pointer hover:underline">
+            Read more
+          </span>
+        </CardDescription>
       </CardHeader>
+      <EllipsisVertical
+        className="absolute top-2 right-2 cursor-pointer"
+        size={20}
+      />
     </Card>
   );
 };
