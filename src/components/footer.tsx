52de206e6c964e708b87631387894928af434a09
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-05-14 20:50:17 +0800

    feat: add description in footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date(Date.now()).getFullYear();
   return (
-    <div className="border-t py-3 flex items-center justify-center">Footer</div>
+    <div className="border-t py-3 flex items-center justify-center">{`All rights reserved ${currentYear}`}</div>
   );
 };
 
